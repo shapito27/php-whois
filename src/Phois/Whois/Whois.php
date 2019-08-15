680868921946035c98ac54621be0882de09c4678
shapito27 <legionerust@yandex.ru>
2019-08-15 18:31:36 +0300

    New method data() to parse the domain info.

--- src/Phois/Whois/Whois.php
+++ src/Phois/Whois/Whois.php
@@ -29,8 +29,14 @@ class Whois
             throw new \InvalidArgumentException("Invalid $domain syntax");
         // setup whois servers array from json file
         $this->servers = json_decode(file_get_contents( __DIR__.'/whois.servers.json' ), true);
+        
+        if (!$this->isValid())
+        	throw new \InvalidArgumentException("Domain name isn't valid!");
     }
-
+    
+    /**
+     * @param string, domain whois information
+     */
     public function info()
     {
         if ($this->isValid()) {
@@ -125,6 +131,88 @@ class Whois
         }
     }
 
+    public function data()
+    {
+      $result = new \stdClass();
+      $result->status = 0;
+      $result->message = 'error';
+      $result->data = [];
+      try {
+        $info = $this->info();
+
+        $not_found_string = FALSE;
+        if (isset($this->servers[$this->TLDs][1])) {
+           $not_found_string = $this->servers[$this->TLDs][1];
+        }
+
+        // Check if this domain is not found (available for registration).
+        if ($not_found_string) {
+          if (strpos($info, $not_found_string) !== false) {
+            $result->status = 2;
+            $result->message = 'not_found';
+          }
+        }
+
+        // Make sure the status is still the default value, and the not_found
+        // string value are exists before extracting the data from info.
+        if (($result->status == 0) && ($not_found_string)) {
+          $exploded_info = explode("\n", $info);
+          $data = [];
+          foreach ($exploded_info as $lineNumber => $line) {
+            if (strpos($line, 'Creation Date:') !== false) {
+              $data['creation_date'] = trim(str_replace('Creation Date:', '', $line));
+            }
+
+            if (strpos($line, 'Registry Expiry Date:') !== false) {
+              $data['expiration_date'] = trim(str_replace('Registry Expiry Date:', '', $line));
+            }
+
+            if (strpos($line, 'Updated Date:') !== false) {
+              $data['update_date'] = trim(str_replace('Updated Date:', '', $line));
+            }
+
+            if (strpos($line, 'Registry Domain ID:') !== false) {
+              $data['registry_domain_id'] = trim(str_replace('Registry Domain ID:', '', $line));
+            }
+
+            if (strpos($line, 'Registrar:') !== false) {
+              if (!isset($data['registrar'])) {
+                $data['registrar'] = [];
+              }
+              $data['registrar']['name'] = trim(str_replace('Registrar:', '', $line));
+            }
+
+            if (strpos($line, 'Registrar IANA ID:') !== false) {
+              if (!isset($data['registrar'])) {
+                $data['registrar'] = [];
+              }
+              $data['registrar']['id'] = trim(str_replace('Registrar IANA ID:', '', $line));
+            }
+
+            if (strpos($line, 'Name Server:') !== false) {
+              if (!isset($data['name_servers'])) {
+                $data['name_servers'] = [];
+              }
+              $data['name_servers'][] = trim(str_replace('Name Server:', '', $line));
+            }
+          }
+
+          // If there are data, we will count this as registered.
+          if (count($data) > 0) {
+            $result->status = 1;
+            $result->message = 'found';
+            $result->data = $data;
+          }
+        }
+
+      } catch (Exception $e) {
+        $result->status = -1;
+        $result->message = 'exception';
+      }
+
+      return $result;
+    }
+
     public function htmlInfo()
     {
         return nl2br($this->info());
@@ -153,7 +241,10 @@ class Whois
     {
         return $this->subDomain;
     }
-
+    
+	/**
+     * @return boolean, true for domain avaliable, false for domain registered
+     */
     public function isAvailable()
     {
         $whois_string = $this->info();
